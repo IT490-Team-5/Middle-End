a824861c4635e71d4f06833f657f326882a20fc7
teoizkrov <ted.krastev@gmail.com>
2021-03-22 02:35:48 -0700

    more fluff changed

--- middle/server.py
+++ middle/server.py
@@ -1,15 +1,16 @@
 import pika, sys, os
 
+
+def cb(ch, method, properties, body):
+        print(">%s" % body.decode("utf-8"))
+
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-
     channel.queue_declare(queue='hello')
 
-    def callback(ch, method, properties, body):
-        print(">%s:" % body.decode("utf-8"))
+    channel.basic_consume('hello', cb, auto_ack=True)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
     print('>Waiting for messages..')
     channel.start_consuming()
