6ee25c5f08232bd3a939b81ddbe2e1f72c7d64d6
teoizkrov <ted.krastev@gmail.com>
2021-03-23 15:49:53 -0700

    receive from db added

--- middle/server.py
+++ middle/server.py
@@ -5,18 +5,17 @@ def callback(ch, method, properties, body):
 	r = json.loads(body)	
 	d = {}
 
-	# here, the front end is sending me either login verification or registering
+	# here, the front end is sending me either login verification or registering. I send to backend.
 	if r.get("reason") == "create" or r.get('reason') == "login":
 		channel.basic_publish(exchange="", routing_key="right", body=body)
 
-	# here, the backend is sending me a generated response query (after front end). 
+	# here, the backend is sending me a generated response query (after front end). I send to db.
 	elif r.get("reason") == "query":
-		r["reason"] = "sqlstatement"	
-		res = json.dumps(r)
-		channel.basic_publish(exchange="", routing_key="database", body=res)
-
+		channel.basic_publish(exchange="", routing_key="database", body=body)
+	
+	# here, the database is sending me the results of the query. I send this to front.
 	elif r.get("reason") == "results":
-		pass
+		channel.basic_publish(exchange="", routing_key="front", body=body)
 	else:
 		print("I don't understand")
 
