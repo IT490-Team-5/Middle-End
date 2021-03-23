3665273f44d3bb9e21781d6c18fa07d907d03ac0
teoizkrov <ted.krastev@gmail.com>
2021-03-22 18:15:24 -0700

    loads commit with body from consumer. not the best

--- middle/server.py
+++ middle/server.py
@@ -1,26 +1,20 @@
-import pika, sys, os
+import pika
+import json
+
+def callback(ch, method, properties, body):
+	try:
+        print(loads(body))
+	except:
+		print(body)
+		
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_consume('hello', callback, auto_ack=True)
 
 
-def cb(ch, method, properties, body):
-        print(">%s" % body.decode("utf-8"))
+print('>Waiting for messages..')
+channel.start_consuming()
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='hello')
-
-    channel.basic_consume('hello', cb, auto_ack=True)
-
-
-    print('>Waiting for messages..')
-    channel.start_consuming()
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
