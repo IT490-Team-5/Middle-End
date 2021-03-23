5bf7987e3149f44279b1b48734e66a05f6173417
teoizkrov <ted.krastev@gmail.com>
2021-03-23 11:25:31 -0700

    changed json loads

--- middle/server.py
+++ middle/server.py
@@ -2,19 +2,14 @@ import pika
 import json
 
 def callback(ch, method, properties, body):
-	try:
-        print(loads(body))
-	except:
-		print(body)
-		
+	print(json.loads(body))	
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 channel.queue_declare(queue='hello')
-
 channel.basic_consume('hello', callback, auto_ack=True)
 
 
-print('>Waiting for messages..')
+print('Waiting for messages..')
 channel.start_consuming()
 
